b3d1e7508366e60d8a75d5e1541e13f6c427d400
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-06-20 15:51:39 +0530

    Add files via upload

---
+++ Data types/1st.py
@@ -0,0 +1,13 @@
+a = 4                 # Integer data type
+b = 4.7               # Float data type   
+c = "Pankaj"          # String data type
+d = False             # Boolean data type
+e = 3 + 4j            # Complex data type
+f = None              # None data type
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)    
