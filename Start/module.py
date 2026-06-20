5ee190fe3306859c0fe0d7b5066a065b1471eb8f
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-06-20 15:50:37 +0530

    Add files via upload

---
+++ Start/module.py
@@ -0,0 +1,18 @@
+import pyjokes
+
+# print("printing jokes")
+
+# this prints a random jok
+
+
+
+"""
+This is a simple script to print a random joke.
+this will not do nothing else, just print a joke.
+"""
+
+'''hello
+pankaj
+'''
+joke = pyjokes.get_joke()
+print(joke)
