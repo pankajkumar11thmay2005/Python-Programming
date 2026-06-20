9026a71a0851ffe5d1ed66b4cbd9a2f2fe7dd6c3
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-06-20 15:52:17 +0530

    Add files via upload

---
+++ Input() Function/1st.py
@@ -0,0 +1,14 @@
+# Input() Function
+# The input() function is used to take input from the user. It takes a string as an argument, which is displayed as a prompt to the user. The function returns the input as a string.
+
+a = input("Enter Number 1: ")  
+b = input("Enter Number 2: ")
+
+print("Number 1 is: ", a)
+print("Number 2 is: ", b)
+
+print("Sum is : ", a + b)
+
+a = 7
+b = 3
+print("Sum is : ", a + b)
